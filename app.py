7762752493c14207a3974d058a68864f094b4760
fitrahfajar14 <108163447+fitrahfajar14@users.noreply.github.com>
2022-09-07 16:37:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import paho.mqtt.client as paho
 from paho import mqtt
-#from relay import set_relay
-# from servo import set_servo_1,set_servo_2
+from relay import set_relay
+from servo import set_servo_1,set_servo_2
 import RPi.GPIO as GPIO
 
 # define static variable
@@ -16,8 +16,8 @@ password = 'BBFF-msUz1oCTyANwP7pOhefc56jKQAUKD7'
 
 # topic subscribe
 relay_topic = '/v1.6/devices/control_sensor/relay/lv' # -> contoh : /v1.6/devices/device_01/relay/lv
-# servo_1_topic = '/v1.6/devices/control_sensor/servo_1/lv'
-# servo_2_topic = '/v1.6/devices/control_sensor/servo_2/lv'
+servo_1_topic = '/v1.6/devices/control_sensor/servo_1/lv'
+servo_2_topic = '/v1.6/devices/control_sensor/servo_2/lv'
 
 # inisiasi relay
 relay = 16
@@ -31,8 +31,8 @@ def on_connect(client, userdata, flags, rc):
     # Subscribing in on_connect() - if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe(relay_topic)
-#     client.subscribe(servo_1_topic)
-#     client.subscribe(servo_2_topic)
+    client.subscribe(servo_1_topic)
+    client.subscribe(servo_2_topic)
  
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
@@ -41,29 +41,29 @@ def on_message(client, userdata, msg):
 
     if payload_decoded != '':
 
-     if msg.topic == relay_topic:
-        if float(payload_decoded) == 0.0:
-            print("matikan relay")
-            GPIO.output(relay, GPIO.LOW)
-        if float(payload_decoded) == 1.0:
-            print("nyalakan relay")
-            GPIO.output(relay, GPIO.HIGH)
+      if msg.topic == relay_topic:
+         if float(payload_decoded) == 0.0:
+             print("matikan relay")
+             GPIO.output(relay, GPIO.LOW)
+         if float(payload_decoded) == 1.0:
+             print("nyalakan relay")
+             GPIO.output(relay, GPIO.HIGH)
 
-#      if msg.topic == servo_1_topic:
-#         if float(payload_decoded) == 0.0:
-#             print("matikan servo 1")
-#             set_servo_1("low")
-#         if float(payload_decoded) == 1.0:
-#             print("nyalakan servo 1")
-#             set_servo_1("high")
+      if msg.topic == servo_1_topic:
+         if float(payload_decoded) == 0.0:
+             print("matikan servo 1")
+             set_servo_1("low")
+         if float(payload_decoded) == 1.0:
+             print("nyalakan servo 1")
+             set_servo_1("high")
              
-#     if msg.topic == servo_2_topic:
-#         if float(payload_decoded) == 0.0:
-#             print("matikan servo 2")
-#             set_servo_2("low")
-#         if float(payload_decoded) == 1.0:
-#             print("nyalakan servo 2")
-#             set_servo_2("high")
+     if msg.topic == servo_2_topic:
+         if float(payload_decoded) == 0.0:
+             print("matikan servo 2")
+             set_servo_2("low")
+         if float(payload_decoded) == 1.0:
+             print("nyalakan servo 2")
+             set_servo_2("high")
         
 # Create an MQTT client and attach our routines to it.
 client = paho.Client()
